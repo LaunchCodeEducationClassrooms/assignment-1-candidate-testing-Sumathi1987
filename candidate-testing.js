6ed1484e3692d99f2317eb8803615d20ffe4e49d
Sumathiponniah <thasel@gmail.com>
2021-02-18 22:19:02 +0000

    completed to display the grade

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,15 +5,15 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "; ;
-let correctAnswer = "Sally Ride"; ;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions =["Who was the first American woman in space? " ,"True or false: 5000 meters = 5 kilometers.", "(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers= [];
-let grade ;
-let numberOfCorrectAnswer =0 ;
-let result ;
+let grade;
+let totalCorrectAnswer =0;
+let result;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -26,9 +26,9 @@ for (let i = 0; i < questions.length; i++) {
 candidateAnswers[i] = input.question(questions[i]);
 console.log(`your answer : ${candidateAnswers[i]}`);
 console.log(`Correct Answer : ${correctAnswers[i]}`);
-if (candidateAnswers[i] == correctAnswers[i].toLowerCase()) {
+if (candidateAnswers[i] == correctAnswers[i].toLowerCase()){
      candidateAnswers[i].toLowerCase();
-     numberOfCorrectAnswer++;
+     totalCorrectAnswer++;
      
 }
 }
@@ -38,8 +38,8 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
- grade = numberOfCorrectAnswer / (questions.length) * 100
- console.log(`>>> Overall Grade: ${grade} (${numberOfCorrectAnswer} of ${questions.length} responses correct) <<<`);
+ grade = totalCorrectAnswer / (questions.length) * 100 ;
+ console.log(`>>> Overall Grade: ${grade} (${totalCorrectAnswer} of ${questions.length} responses correct) <<<`);
 
   if (grade >= 80 ) {
     result = "PASSED";
