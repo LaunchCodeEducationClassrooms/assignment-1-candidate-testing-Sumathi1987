f6d48bf084c1b6d9087fcb34c133e35f50c81f48
Sumathiponniah <thasel@gmail.com>
2021-02-19 03:41:15 +0000

    trying to commit

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,17 +7,17 @@ let candidateName='';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space?";
 let correctAnswer="Sally Ride";
-let candidateAnswer='';
-let questions =["Who was the first American woman in space? ","True or false: 5 kilometers = 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let candidateAnswer="";
+let questions =["Who was the first American woman in space? ","True or false: 5 kilometer = 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
 let correctAnswers=["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers=[];
 let grade;
 let totalCorrectAnswer=0;
-let result;
+//let result;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName=input.question('candidate Name : ');
+candidateName=input.question("candidate Name : ");
 }
 
 function askQuestion() {
@@ -27,7 +27,7 @@ for(let i=0;i<questions.length;i++)
 candidateAnswers[i]=input.question(questions[i]);
 console.log(`your answer : ${candidateAnswers[i]}`);
 console.log(`Correct Answer : ${correctAnswers[i]}`);
-if(candidateAnswers[i].toLowerCase()==correctAnswers[i].toLowerCase()){
+if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
      totalCorrectAnswer++;   
 }
 }
@@ -40,7 +40,7 @@ function gradeQuiz(candidateAnswers) {
  grade=totalCorrectAnswer/(questions.length)*100 ;
  console.log(`>>> Overall Grade: ${grade} (${totalCorrectAnswer} of ${questions.length} responses correct) <<<`);
 
-  if(grade>=80)
+  /*if(grade>=80)
   {
     result="PASSED";
   }else{
@@ -48,8 +48,13 @@ function gradeQuiz(candidateAnswers) {
   } 
 console.log(`>>> Status: ${result} <<<`);
  return grade;
-  }
-
+}*/
+ if(grade >= 80){
+      console.log(">>> Status: PASSED <<<");
+    } else {
+      console.log(">>> Status: FAILED <<<");
+    }
+}
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
